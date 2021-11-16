4d75f4931b0293941eef9234434b15fdc4792906
Dévald Tari <devald.tari@gmail.com>
2021-11-16 13:03:24 +0100

    Ignore errors, new date format and unique id generation

--- music-dl.fish
+++ music-dl.fish
@@ -2,12 +2,13 @@ function music-dl
   if test -z $argv
     echo 'Please give me a file that contains the URL(s)'
   else
-    set FOLDER $HOME/Music/(date +%Y-%m-%d_%H-%M)-(date +%S | md5 | cut -c 1-4)
+    set FOLDER $HOME/Music/(date +"%m-%d-%Y")-(uuidgen | cut -d - -f 1)
     mkdir -p $FOLDER
     youtube-dl --batch-file $argv \
                --extract-audio \
                --audio-format mp3 \
                --audio-quality 0 \
+               --ignore-errors \
                --output "$FOLDER/%(title)s.%(ext)s"
   end
 end
