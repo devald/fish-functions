13cd9f9a5d405c479d719ab4e9ab12c8270cf619
Dévald Tari <devald.tari@gmail.com>
2021-11-25 20:49:59 +0100

    Parallel download

--- music-dl.fish
+++ music-dl.fish
@@ -1,14 +1,20 @@
 function music-dl
   if test -z $argv
+    set current_command (status current-command)
     echo 'Please give me a file that contains the URL(s)'
+    echo "Usage: $current_command somefile.txt"
   else
+    set MAXPROCS (sysctl -n hw.ncpu)
     set FOLDER $HOME/Music/(date +"%m-%d-%Y")-(uuidgen | cut -d - -f 1)
     mkdir -p $FOLDER
-    youtube-dl --batch-file $argv \
-               --extract-audio \
+    xargs -I {} -P $MAXPROCS \
+    youtube-dl --extract-audio \
                --audio-format mp3 \
                --audio-quality 0 \
+               --restrict-filenames \
                --ignore-errors \
-               --output "$FOLDER/%(title)s.%(ext)s"
+               --no-mtime \
+               --output "$FOLDER/%(title)s.%(ext)s" \
+               {} < $argv
   end
 end
